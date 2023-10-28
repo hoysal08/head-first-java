378b3d312f1c6b5725d72d0fe003c985684db055
Sooraj Hoysal <surajhoysal817@gmail.com>
2023-10-28 12:45:41 +0530

    first commit

---
+++ 1/Be_The_Compiler.java
@@ -0,0 +1,40 @@
+/*
+ * class Exercise1b {
+ * public static void main(String [] args) {
+ * int x = 1;
+ * while ( x < 10 ) {
+ * if ( x > 3) { System.out.println(“big x”);
+ * } }
+ * } }
+ */
+
+// Solution - The value of x needs to be updated, or else we would endup in a
+// infinite loop
+
+/*
+ * public static void main(String [] args) {
+ * int x = 5;
+ * while ( x > 1 ) {
+ * x = x - 1;
+ * if ( x < 3) {
+ * System.out.println(“small x”);
+ * }
+ * }
+ * }
+ */
+
+// the main function should be present inside a class - mising class definition
+
+/*
+ * class Exercise1b {
+ * int x = 5;
+ * while ( x > 1 ) {
+ * x = x - 1;
+ * if ( x < 3) {
+ * System.out.println(“small x”);
+ * }
+ * }
+ * }
+ */
+
+ // missing function definition, The class doesn't have a main function definition
