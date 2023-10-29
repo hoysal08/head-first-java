7184b2019e1108f1f1b87d0966220eec5cfbe099
Sooraj Hoysal <surajhoysal817@gmail.com>
2023-10-29 15:39:53 +0530

    chapter 2 exercise solution added

---
+++ 2/Be_The_Compiler.java
@@ -0,0 +1,45 @@
+/*
+ * class TapeDeck {
+ * boolean canRecord = false;
+ * void playTape() {
+ * System.out.println("tape playing");
+ * }
+ * 
+ * void recordTape() {
+ * System.out.println("tape recording");
+ * }
+ * }
+ * 
+ * class TapeDeckTestDrive {
+ * public static void main(String [] args) {
+ * t.canRecord = true;
+ * t.playTape();
+ * if (t.canRecord == true) {
+ * t.recordTape();
+ * }
+ * }
+ * }
+ * 
+ * //The object t is not created, we can create the object using TapeDeck t = new TapeDeck( );
+ */
+
+/*
+ * class DVDPlayer {
+ * boolean canRecord = false;
+ * void recordDVD() { System.out.println(“DVD recording”);
+ * }
+ * }
+ * 
+ * class DVDPlayerTestDrive {
+ * public static void main(String [] args) {
+ * DVDPlayer d = new DVDPlayer();
+ * d.canRecord = true;
+ * d.playDVD();
+ * if (d.canRecord == true) {
+ * d.recordDVD();
+ * }
+ * }
+ * }
+ * 
+ * //The method playDVD() is not defined in the DVDPlayer class, we can define the method in the DVDPlayer class.
+ */
