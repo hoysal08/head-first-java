17f9f2a112d5fcbabe3c3a34e06ab2cde0819fc8
Sooraj Hoysal <surajhoysal817@gmail.com>
2023-10-31 16:14:36 +0530

    chapter 6 exercise added

---
+++ 5/TheGame/GameHelper.java
@@ -0,0 +1,20 @@
+package TheGame;
+
+import java.io.*;
+
+public class GameHelper {
+    public String getUserInput(String prompt) {
+        String inputLine = null;
+        System.out.print(prompt + " ");
+        try {
+            BufferedReader is = new BufferedReader(new InputStreamReader(System.in));
+            inputLine = is.readLine();
+            if (inputLine.isEmpty()) {
+                return null;
+            }
+        } catch (IOException e) {
+            System.out.println("IOException: " + e);
+        }
+        return inputLine;
+    }
+}
