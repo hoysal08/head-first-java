325916a23527a3fbec32c6cf2811787f3133f72f
Sooraj Hoysal <surajhoysal817@gmail.com>
2023-11-07 16:25:50 +0530

    added solution for exercise 17

---
+++ 17/TorF.java
@@ -0,0 +1,33 @@
+/*
+ * 1. The Java compiler has a flag, -d, that lets you decide where your .class
+ * files should go. -> True
+ * 2. A JAR is a standard directory where your .class files
+ * should reside. -> False
+ * 3. When creating a Java Archive you must create a file called jar.mf. ->
+ * False
+ * 4. The supporting file in a Java Archive declares which class has the main()
+ * method. -> True
+ * 5. JAR files must be unzipped before the JVM can use the classes inside. ->
+ * False
+ * 6. At the command line, Java Archives are invoked using the -arch flag. ->
+ * False
+ * 7. Package structures are meaningfully represented using hierarchies. -> True
+ * 8. Using your company’s domain name is not recommended when naming packages.
+ * -> False
+ * 9. Different classes within a source file can belong to different packages.->
+ * False
+ * 10. When compiling classes in a package, the -p flag is highly recommended.
+ * -> False
+ * 11. When compiling classes in a package, the full name must mirror the
+ * directory tree. ->True
+ * 12. Judicious use of the -d flag can help to assure that
+ * there are no typos in your class tree. -> True
+ * 13. Extracting a JAR with packages will create a directory called meta-inf.
+ * -> True
+ * 14. Extracting a JAR with packages will create a file called manifest.mf.
+ * ->True
+ * 15. The JWS helper app always runs in conjunction with a browser. False
+ * 16. JWS applications require a .nlp (Network Launch Protocol) file to work
+ * properly. -> False
+ * 17. A JWS’s main method is specified in its JAR file. -> False
+ */
